de3b19459dbd732db5b8a3183e51ca3b5726888d
Mike Xu <mxu11290@gmail.com>
2018-05-21 23:59:30 -0400

    scaffold out endpoint to issue a cert

--- common/models/app-id.js
+++ common/models/app-id.js
@@ -1,4 +1,24 @@
-'use strict';
-
 module.exports = function(Appid) {
-};
+  Appid.issue = (id, email, schema, cb) => {
+    console.log(id, email, schema)
+    cb(null, true)
+  }
+
+  Appid.remoteMethod('issue', {
+    http: { path: '/:id/issue', verb: 'post' },
+    accepts: [
+      { arg: 'id', type: 'string', required: true },
+      {
+        arg: 'email',
+        type: 'string',
+        required: true,
+      },
+      {
+        arg: 'schema',
+        type: 'object',
+        required: true,
+      },
+    ],
+    returns: { arg: 'success', type: 'boolean' },
+  })
+}
