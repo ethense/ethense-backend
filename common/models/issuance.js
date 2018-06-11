51eaf414bde3ee1af3c9b3ab8da5629f499e7509
Mike Xu <mxu11290@gmail.com>
2018-06-11 19:19:46 -0400

    outline endpoint for executing batch issuance

--- common/models/issuance.js
+++ common/models/issuance.js
@@ -1,2 +1,19 @@
+const app = require('../../server/server')
+
 module.exports = function(Issuance) {
+  Issuance.batchIssue = async (id, cb) => {
+    const issuance = await app.models.Issuance.findById(id)
+    if(issuance.batchIssuing) {
+      const error = new Error('already executing batch issuance')
+      error.status = 409 // Conflict
+      throw error
+    }
+    await issuance.updateAttributes({ batchIssuing: true })
+    return issuance
+  }
+  Issuance.remoteMethod('batchIssue', {
+    http: { path: '/:id/batchIssue', verb: 'get' },
+    accepts: [{ arg: 'id', type: 'string', required: true }],
+    returns: { arg: 'issuance', type: 'object' },
+  })
 }
