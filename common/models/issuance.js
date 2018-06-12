8cc0a47f838db4ce3f536a8e7076221456d7d231
Mike Xu <mxu11290@gmail.com>
2018-06-12 01:52:28 -0400

    batch issuance queues up jobs to send credential request emails

--- common/models/issuance.js
+++ common/models/issuance.js
@@ -1,19 +1,73 @@
+const kue = require('kue')
+const queue = kue.createQueue()
+
 const app = require('../../server/server')
+
+queue.process('credentialRequestEmail', async (job, done) => {
+  const issuance = await app.models.Issuance.findById(job.data.issuanceId)
+
+  const numRecipients = issuance.recipients.length
+
+  async function next(i) {
+    const recipient = issuance.recipients[i]
+    console.log('send email to', recipient)
+    const status = Math.random() < 0.1 ? 'request failed' : 'requested'
+    if (recipient.email) {
+      job.log(`sending email to ${recipient.email}`)
+      await issuance.updateAttributes({
+        recipients: issuance.recipients.map(
+          (r, j) =>
+            j === i
+              ? {
+                  ...r,
+                  status,
+                  lastUpdated: Math.floor(new Date() / 1000),
+                }
+              : r
+        ),
+      })
+      job.log('email sent')
+    } else {
+      console.error('recipient missing email', recipient)
+    }
+    setTimeout(async () => {
+      if (i === numRecipients - 1) done()
+      else next(i + 1)
+    }, 1000)
+  }
+
+  next(0)
+})
 
 module.exports = function(Issuance) {
   Issuance.batchIssue = async (id, cb) => {
     const issuance = await app.models.Issuance.findById(id)
-    if(issuance.batchIssuing) {
+    if (issuance.batchIssuing) {
       const error = new Error('already executing batch issuance')
       error.status = 409 // Conflict
       throw error
     }
     await issuance.updateAttributes({ batchIssuing: true })
+
+    const job = queue
+      .create('credentialRequestEmail', {
+        issuanceId: id,
+        instance: issuance,
+      })
+      .removeOnComplete(true)
+      .save()
+
+    const handleDone = id => async result => {
+      console.log('done', result, id)
+      const issuance = await app.models.Issuance.findById(id)
+      issuance.updateAttributes({ done: true, batchIssuing: false })
+    }
+    job.on('complete', handleDone(id)).on('failed', handleDone(id))
     return issuance
   }
   Issuance.remoteMethod('batchIssue', {
     http: { path: '/:id/batchIssue', verb: 'get' },
     accepts: [{ arg: 'id', type: 'string', required: true }],
-    returns: { arg: 'issuance', type: 'object' },
+    returns: { arg: 'root', type: 'object' },
   })
 }
