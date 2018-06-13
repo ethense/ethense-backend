d27e29f231f4147fe535cf4aec64f4d9d44ed9ea
Mike Xu <mxu11290@gmail.com>
2018-06-13 11:23:44 -0400

    move credential request email from AppId endpoint to job queue in Issuance

--- common/models/issuance.js
+++ common/models/issuance.js
@@ -1,5 +1,11 @@
 const kue = require('kue')
 const app = require('../../server/server')
+const uport = require('uport')
+const qr = require('qr-image')
+const nodemailer = require('nodemailer')
+const fs = require('fs')
+
+const server = app.get('server')
 
 const redisConf = app.get('redis')
 const redisPassword = redisConf.password ? `:${redisConf.password}@` : ''
@@ -15,37 +21,146 @@ const queue = kue.createQueue({
   },
 })
 
+const DEFAULT_TEMPLATE = (appName, qr, uri) =>
+  `<div>Congratulations! ${appName} would like to issue you a certificate.  Scan the QR code with uPort to receive it.<img src="${qr}"></img><a href="${uri}">For mobile, click to open uPort.</a>  Experiencing issues?  Visit out <a href="https://consensysteam.atlassian.net/servicedesk/customer/portal/1">helpdesk</a></div>`
+
 queue.process('credentialRequestEmail', async (job, done) => {
   const issuance = await app.models.Issuance.findById(job.data.issuanceId)
+  const claimTemplate = await app.models.ClaimTemplate.findById(
+    issuance.claimId
+  )
+  const appId = await app.models.AppId.findById(issuance.appId)
+
+  const credentials = new uport.Credentials({
+    appName: appId.name,
+    address: appId.mnid,
+    signer: uport.SimpleSigner(appId.privateKey),
+  })
+
+  const transport = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: app.get('email').user,
+      pass: app.get('email').password,
+    },
+  })
 
   const numRecipients = issuance.recipients.length
 
+  function getAttributeValue(recipient, attribute) {
+    let value = null
+    switch (attribute.type) {
+      case 'string':
+        value = attribute.value
+        break
+      case 'dynamic':
+        value =
+          attribute.value.toLowerCase() === 'email'
+            ? recipient.email
+            : recipient.data[attribute.value]
+        break
+      case 'object':
+        value = attribute.children.reduce((acc, a) => {
+          acc[a.name] = getAttributeValue(recipient, a)
+          return acc
+        }, {})
+        break
+      default:
+        console.error('unsupported attribute type', attribute.type)
+    }
+    return value
+  }
+
   async function next(i) {
     const recipient = issuance.recipients[i]
-    const status = Math.random() < 0.1 ? 'request failed' : 'requested'
-    if (recipient.email) {
-      console.log(`sending email to ${recipient.email}`)
-      job.log(`sending email to ${recipient.email}`)
-      await issuance.updateAttributes({
-        recipients: issuance.recipients.map(
-          (r, j) =>
-            j === i
-              ? {
-                  ...r,
-                  status,
-                  lastUpdated: Math.floor(new Date() / 1000),
-                }
-              : r
-        ),
+    let status = 'request failed'
+    let filename = null
+
+    try {
+      // generate claim from schema and recipient data
+      const claimBody = claimTemplate.schema.reduce((acc, a) => {
+        acc[a.name] = getAttributeValue(recipient, a)
+        return acc
+      }, {})
+      const claim = { [claimTemplate.name]: claimBody }
+      console.log('claim', claim)
+
+      // create a pending claim
+      const pendingClaim = await app.models.PendingClaim.create({
+        issuerAppId: appId.id,
+        claim,
+        issuanceId: issuance.id,
+        recipientEmail: recipient.email,
       })
-      job.log('email sent')
-    } else {
-      console.error('recipient missing email', recipient)
+      console.log('pendingClaim', pendingClaim)
+
+      // create credential request
+      const callbackUrl = `${server.host}:${server.backendPort}${
+        server.basePath
+      }/api/PendingClaims/${pendingClaim.id}/collect`
+      const requestToken = await credentials.createRequest({
+        callbackUrl,
+        notifications: true,
+        exp: Math.floor(Date.now() / 1000) + 31557600, // 1 year
+      })
+      console.log('requestToken', requestToken)
+
+      // encode request in QR image
+      const requestUri = `me.uport:me?requestToken=${requestToken}`
+      const deepLink = `https://id.uport.me/me?requestToken=${requestToken}&callback_type=post`
+      filename = `QR-${issuance.id}-${i}.png`
+      const requestQrData = qr.image(requestUri, { type: 'png' })
+      await new Promise((resolve, reject) => {
+        requestQrData.pipe(fs.createWriteStream(filename)).on('finish', () => {
+          return resolve(filename)
+        })
+      })
+
+      // send email
+      const emailOptions = {
+        from: 'Ethense',
+        to: recipient.email,
+        subject: `${issuance.name} Certificate`,
+        html: DEFAULT_TEMPLATE(appId.name, `cid:${filename}`, deepLink),
+        attachments: [{ filename, path: `${filename}`, cid: filename }],
+      }
+      console.log(`sending email to ${recipient.email}`, emailOptions)
+
+      const mailInfo = await new Promise((resolve, reject) => {
+        transport.sendMail(emailOptions, (error, info) => {
+          if (error) return reject(error)
+          return resolve(info)
+        })
+      })
+      console.log('mailInfo', mailInfo)
+      status = 'requested'
+    } catch (error) {
+      console.error(`error sending email to ${recipient.email}`)
     }
-    setTimeout(async () => {
-      if (i === numRecipients - 1) done()
-      else next(i + 1)
-    }, 1000)
+
+    if(filename) {
+      await new Promise((resolve, reject) => {
+        fs.unlink(filename, error => {
+          if(error) return reject(error)
+          return resolve(true)
+        })
+      })
+    }
+
+    await issuance.updateAttributes({
+      recipients: issuance.recipients.map(
+        (r, j) =>
+          j === i
+            ? {
+                ...r,
+                status,
+                lastUpdated: Math.floor(new Date() / 1000),
+              }
+            : r
+      ),
+    })
+    if (i === numRecipients - 1) done()
+    else next(i + 1)
   }
 
   next(0)
--- common/models/pending-claim.js
+++ common/models/pending-claim.js
@@ -19,44 +19,51 @@ const getValues = parent => attr => {
 
 module.exports = function(Pendingclaim) {
   Pendingclaim.collect = async (id, data, cb) => {
-    try {
-      // find the claim
-      console.log(id)
-      const pendingClaim = await app.models.PendingClaim.findById(id)
-      console.log(pendingClaim.id)
-      // get the appId from the claim
-      const appId = await app.models.AppId.findOne({
-        id: pendingClaim.issuerAppId,
-      })
-      // set up uport
-      const credentials = new uport.Credentials({
-        appName: appId.name,
-        address: appId.mnid,
-        signer: uport.SimpleSigner(appId.privateKey),
-      })
-      console.log(credentials)
-      // get their mnid from response
-      console.log(data)
-      const identity = await credentials.receive(data.access_token)
-      console.log(identity)
-      // get the claim data from schema
-      const claim = {
-        title: {},
-      }
-      pendingClaim.schema.forEach(getValues(claim.title))
-      console.log(claim)
-      const attestation = await credentials.attest({
-        sub: identity.address,
-        claim,
-      })
-      const attestationUri = `me.uport:add?attestations=${attestation}`
-      await credentials.push(identity.pushToken, identity.publicEncKey, {
-        url: attestationUri,
-      })
-      cb(null, true)
-    } catch (error) {
-      cb(error, null)
-    }
+    // find the claim
+    console.log(id)
+    const pendingClaim = await app.models.PendingClaim.findById(id)
+    console.log(pendingClaim.id)
+    // get the appId from the claim
+    const appId = await app.models.AppId.findOne({
+      id: pendingClaim.issuerAppId,
+    })
+    // set up uport
+    const credentials = new uport.Credentials({
+      appName: appId.name,
+      address: appId.mnid,
+      signer: uport.SimpleSigner(appId.privateKey),
+    })
+    console.log(credentials)
+    // get their mnid from response
+    console.log(data)
+    const identity = await credentials.receive(data.access_token)
+    console.log(identity)
+    // get the claim data from schema
+    const attestation = await credentials.attest({
+      sub: identity.address,
+      claim: pendingClaim.claim,
+    })
+    const attestationUri = `me.uport:add?attestations=${attestation}`
+    await credentials.push(identity.pushToken, identity.publicEncKey, {
+      url: attestationUri,
+    })
+
+    const issuance = await app.models.Issuance.findById(pendingClaim.issuanceId)
+    await issuance.updateAttributes({
+      recipients: issuance.recipients.map(
+        r =>
+          r.email === pendingClaim.recipientEmail
+            ? {
+                ...r,
+                status: 'collected',
+                lastUpdated: Math.floor(new Date() / 1000),
+                mnid: identity.address,
+                pushToken: identity.pushToken,
+              }
+            : r
+      ),
+    })
+    return true
   }
 
   Pendingclaim.remoteMethod('collect', {
